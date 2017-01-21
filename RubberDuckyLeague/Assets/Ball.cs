e99c0a7a4a0cf3d119a3afd5d1a4ca47d124e455
manelis <maneliss@gmail.com>
2017-01-21 00:23:23 +0000

    stuff

--- RubberDuckyLeague/Assets/Ball.cs
+++ RubberDuckyLeague/Assets/Ball.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
+	Rigidbody2D self_rigidbody;
+
 	// Use this for initialization
 	void Start () {
-	
+		self_rigidbody = gameObject.GetComponent<Rigidbody2D> ();
 	}
-	
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.GetComponent<Player> () != null) {
+			Vector3 direction = transform.position - collision.gameObject.transform.position;
+			self_rigidbody.constraints = RigidbodyConstraints2D.None;
+			self_rigidbody.AddForce (new Vector2 (direction.x, direction.y) * 500);
+		}
+		else if(collision.gameObject.layer == LayerMask.NameToLayer("MapWater")){
+			self_rigidbody.constraints = RigidbodyConstraints2D.FreezePositionX;
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+
+		RaycastHit2D raycastDown = Physics2D.Raycast (new Vector2 (transform.position.x, transform.position.y), new Vector2 (0, -1.0f),1000.0f, 1 << LayerMask.NameToLayer("MapWater"));
 	}
 }
--- RubberDuckyLeague/Assets/Player.cs
+++ RubberDuckyLeague/Assets/Player.cs
@@ -8,6 +8,11 @@ public class Player : MonoBehaviour {
 	Rigidbody2D rigidbodyPlayer;
 
 	public int playerSpeed = 15;
+	public int playerVerticalSpeed = 30;
+	public float click_duration = 1;
+
+	private float click_delta_time = 0;
+	private bool clicked = false;
 
 	// Use this for initialization
 	void Start () {
@@ -18,9 +23,43 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		float movementAxis = 0;
+		bool fire_button_pressed = false;
+
+		if (clicked)
+			click_delta_time += Time.deltaTime;
+
+		if (click_delta_time > click_duration)
+			click_delta_time = click_duration;
+
+		if (Input.GetButtonDown ("Jump"))
+			clicked = true;
+		if (Input.GetButtonUp ("Jump")) {
+			click_delta_time = 0;
+			clicked = false;
+		}
+		
+		if (playername == "player1")
+		{
+			movementAxis = Input.GetAxis ("Horizontal");
+			fire_button_pressed = Input.GetButton ("Jump");
+		}
+		else {
+			movementAxis = Input.GetAxis ("Horizontal2");
+			fire_button_pressed = Input.GetButton ("Jump2");
+		}
+
+		float horizontal = movementAxis * Time.deltaTime * playerSpeed;
+		float vertical = 0;
+
+		if(fire_button_pressed) 
+			vertical = -(click_duration - click_delta_time) * playerVerticalSpeed;
+
 		RaycastHit2D raycastDown = Physics2D.Raycast (new Vector2 (transform.position.x, transform.position.y), new Vector2 (0, -1.0f),1000.0f, 1 << LayerMask.NameToLayer("MapWater"));
 		RaycastHit2D raycastUp = Physics2D.Raycast (new Vector2 (transform.position.x, transform.position.y), new Vector2 (0, 1.0f),1000.0f, 1 << LayerMask.NameToLayer("MapWater"));
+
+		bool underwater = raycastUp.distance > 0f;
 
 		float sea_y = 0;
 		if (raycastDown.collider != null) {
@@ -29,19 +68,14 @@ public class Player : MonoBehaviour {
 			sea_y = raycastUp.point.y;	
 		}
 
-		transform.position = new Vector3(transform.position.x, sea_y, transform.position.z);
-
-
-		float movementAxis = 0;
-
-		if (playername == "player1")
-			movementAxis = Input.GetAxis ("Horizontal");
-		else {
-			movementAxis = Input.GetAxis ("Horizontal2");
+		if (underwater)
+			vertical += 10;
+		Debug.Log (vertical);
+		if (vertical == 0 && (raycastDown.distance < 0.4f && raycastUp.distance == 0 || raycastUp.distance < 0.4f && raycastDown.distance == 0))
+			transform.position = new Vector3 (transform.position.x + horizontal, sea_y, transform.position.z);
+		else if(vertical < 0 || underwater){
+			transform.position = new Vector3 (transform.position.x + horizontal, transform.position.y + vertical*Time.deltaTime, transform.position.z);
 		}
-
-		float horizontal = movementAxis * Time.deltaTime * playerSpeed;
-		transform.position = transform.position += new Vector3 (horizontal, 0, 0);
 
 		///if (horizontal != 0) {
 
